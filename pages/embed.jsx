4e59c7423366529ba09862cdda7bfdbbde481187
Lucas Fernando Amorim <lf.amorim@yahoo.com.br>
2018-07-30 04:24:27 -0300

    randomize

--- pages/embed.jsx
+++ pages/embed.jsx
@@ -51,6 +51,26 @@ export default class Index extends React.Component {
           new RegExp(search, 'i').test(val)));
   }
 
+  static shuffle(array) {
+    let currentIndex = array.length;
+    let temporaryValue;
+    let randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex !== 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
   render() {
     const content = this.props;
     return (<div>
@@ -62,7 +82,7 @@ export default class Index extends React.Component {
         <div className="columns">
           <Filter onChange={(filter) => { this.setState({ filter }); }} />
           <div className="column">
-            {content && _.chunk(this.filter(), 2).map(chunk =>
+            {content && _.chunk(Index.shuffle(this.filter()), 2).map(chunk =>
               (<div className="columns">
                 {chunk.map(profile => (
                   <div key={profile._id} className="column is-half">
